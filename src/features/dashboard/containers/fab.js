7e464a4dd93d09daf926313b2e93ec6fd4d04d56
Oscar Nylander <oscar@hedvig.com>
2018-11-14 17:20:12 +0100

    Fix fab on android :tada:
    also compatible with iOS, woot!

--- src/features/dashboard/containers/fab.js
+++ src/features/dashboard/containers/fab.js
@@ -20,6 +20,12 @@ const styles = StyleSheet.create({
   animatedView: {
     position: 'absolute',
     bottom: 0,
+    ...Platform.select({
+      android: {
+        width: '100%',
+        height: '100%',
+      },
+    }),
   },
 });
 
