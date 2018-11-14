10d05fc212a3bff4c561cef4c30089b27183bf78
Gustaf Gunér <gustaf@guner.se>
2018-11-14 15:16:03 +0100

    Minor fix

--- src/components/HedvigLogoTitle.tsx
+++ src/components/HedvigLogoTitle.tsx
@@ -3,21 +3,19 @@ import { Image, Platform } from 'react-native';
 import styled from '@sampettersson/primitives';
 
 const HedvigLogoImage = styled(Image)({
-  hedvigLogo: {
-    width: 96,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    ...Platform.select({
-      android: {
-        marginTop: 5,
-      },
-      ios: {},
-    }),
-  },
+  width: 96,
+  height: 30,
+  justifyContent: 'center',
+  alignItems: 'center',
+  ...Platform.select({
+    android: {
+      marginTop: 5,
+    },
+    ios: {},
+  }),
 });
 
-export const HedvigLogoTitle: React.SFC<{}> = () => (
+export const HedvigLogoTitle = () => (
   <HedvigLogoImage
     source={require('../../assets/identity/hedvig_wordmark/hedvig_wordmark.png')}
   />
