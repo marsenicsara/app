598596cfc50a880fc928758f2648dfcad7c0b9a1
Gustaf Gunér <gustaf@guner.se>
2018-11-21 17:38:31 +0100

    Fragment matcher

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,8 +2,11 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { link } from './link';
+import fm from './fragment-matcher';
 
 export const client = new ApolloClient({
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    fragmentMatcher: fm,
+  }),
   link,
 });
