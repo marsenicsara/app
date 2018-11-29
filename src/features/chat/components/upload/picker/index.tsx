9fa9df7dc4c9c01ba17593e19e973f4f38be6497
Oscar Nylander <oscar@hedvig.com>
2018-11-29 10:00:29 +0100

    Fix images loading as videos on android

--- src/features/chat/components/upload/picker/index.tsx
+++ src/features/chat/components/upload/picker/index.tsx
@@ -33,8 +33,8 @@ interface ListHeaderContextProps {
 }
 
 const ListHeaderContext = React.createContext<ListHeaderContextProps>({
-  sendMessage: () => {},
-  setIsOpen: () => {},
+  sendMessage: () => { },
+  setIsOpen: () => { },
 });
 
 interface ListFooterContext {
@@ -92,7 +92,7 @@ export const Picker: React.SFC<PickerProps> = ({ sendMessage }) => (
                     ListFooterComponent={ListFooterComponent}
                     data={photos!.edges!}
                     renderItem={({ item, index }) =>
-                      item.node.type.includes('Photo') ? (
+                      item.node.type.includes('Photo') || item.node.type.includes('image') ? (
                         <Image
                           uri={item.node.image.uri}
                           isLastInList={index === photos!.edges!.length - 1}
@@ -102,15 +102,15 @@ export const Picker: React.SFC<PickerProps> = ({ sendMessage }) => (
                           }}
                         />
                       ) : (
-                        <Video
-                          uri={item.node.image.uri}
-                          isLastInList={index === photos!.edges!.length - 1}
-                          onUpload={(key) => {
-                            sendMessage(key);
-                            setIsOpen(false);
-                          }}
-                        />
-                      )
+                          <Video
+                            uri={item.node.image.uri}
+                            isLastInList={index === photos!.edges!.length - 1}
+                            onUpload={(key) => {
+                              sendMessage(key);
+                              setIsOpen(false);
+                            }}
+                          />
+                        )
                     }
                     keyExtractor={(item) => String(item.node.image.uri)}
                     onEndReached={() => loadMore()}
