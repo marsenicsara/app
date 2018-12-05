1bb5c68be7371415893d537e3dc0ba5419784f89
Oscar Nylander <oscar@hedvig.com>
2018-12-05 11:25:38 +0100

    Remove some obsolete code

--- src/features/analytics/SegmentTracker.js
+++ src/features/analytics/SegmentTracker.js
@@ -1,6 +1,5 @@
-import { Platform } from 'react-native';
 import Segment from '@hedviginsurance/react-native-analytics-segment-io';
-import Branch, { BranchEvent } from 'react-native-branch';
+import Branch from 'react-native-branch';
 
 export const SemanticEvents = {
   Ecommerce: {
@@ -18,67 +17,12 @@ export const SemanticEvents = {
   },
 };
 
-const branchEventFromSegmentEvent = (event, properties) => {
-  const mapSegmentToBranchEvent = {
-    [SemanticEvents.Ecommerce.CheckoutStarted]: BranchEvent.InitiatePurchase,
-    [SemanticEvents.Ecommerce.PaymentInfoEntered]: BranchEvent.AddPaymentInfo,
-    [SemanticEvents.Ecommerce.OrderCompleted]: BranchEvent.Purchase,
-  };
-
-  const mapSegmentToBranchKeys = {
-    order_id: 'transactionID',
-    affiliation: 'affiliation',
-    revenue: 'revenue',
-    shipping: 'shipping',
-    tax: 'tax',
-    coupon: 'coupon',
-    currency: 'currency',
-    query: 'searchQuery',
-  };
-
-  const branchEventType = mapSegmentToBranchEvent[event];
-
-  const branchProperties =
-    properties &&
-    Object.keys(properties).reduce((acc, segmentKey) => {
-      const branchKey = mapSegmentToBranchKeys[segmentKey];
-      const value = properties[segmentKey];
-      if (branchKey) {
-        acc[branchKey] = value;
-      } else {
-        acc.customData = { ...acc.customData, [segmentKey]: String(value) };
-      }
-      return acc;
-    }, branchEventType ? { description: event } : {});
-
-  return { branchEventType, branchProperties };
-};
-
 export const SegmentTracker = {
-  initialize: ({ androidWriteKey, iosWriteKey, ...options }) => {
-    if (Platform.OS === 'ios') {
-      Segment.setup(iosWriteKey, options);
-    } else if (Platform.OS === 'android') {
-      Segment.setup(androidWriteKey, options).catch(() => {});
-    }
-  },
   identify: (userId, traits) => {
-    if (userId) {
-      Branch.setIdentity(userId);
-    }
     return Segment.identify(userId, traits);
   },
   screen: Segment.screen,
   track: (event, properties) => {
-    const { branchEventType, branchProperties } = branchEventFromSegmentEvent(
-      event,
-      properties,
-    );
-
-    if (branchEventType) {
-      new BranchEvent(branchEventType, null, branchProperties).logEvent();
-    }
-
     return Segment.track(event, properties);
   },
   group: Segment.group,
