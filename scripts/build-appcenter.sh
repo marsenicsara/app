4363db891f0ec0843605b6edc67172a6c1101382
Oscar Nylander <oscar@hedvig.com>
2019-03-22 14:00:41 +0100

    Add some stuff to the AppCenter build
    This might break iOS builds, hehe

--- scripts/build-appcenter.sh
+++ scripts/build-appcenter.sh
@@ -4,3 +4,7 @@ set -uex
 
 scripts/select-firebase-env.sh
 scripts/create-env.sh
+
+npm install -g apollo || true
+../android/gradlew owldroid:updateSchema  || true
+../android/gradlew licenseReleaseReport || true
