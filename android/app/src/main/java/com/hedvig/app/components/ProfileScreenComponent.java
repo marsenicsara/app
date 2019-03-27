ae2188b2f3fb490d3320914d9aaea4f96251af8c
Oscar Nylander <oscar@hedvig.com>
2019-03-27 15:06:46 +0100

    Navigation analytics, last mile

--- android/app/src/main/java/com/hedvig/app/components/ProfileScreenComponent.java
+++ android/app/src/main/java/com/hedvig/app/components/ProfileScreenComponent.java
@@ -1,7 +1,10 @@
 package com.hedvig.app.components;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 
 import com.hedvig.app.R;
@@ -14,6 +17,15 @@ public class ProfileScreenComponent implements ExternalComponent {
         component = new FrameLayout(activity);
         View content = activity.getLayoutInflater().inflate(R.layout.profile_navigation_host, component, false);
         content.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+        content.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(activity);
+                Intent intent = new Intent("profileScreenDidAppear");
+                localBroadcastManager.sendBroadcast(intent);
+                content.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
         component.addView(content);
 
         component.setId(R.id.profile_screen_content);
