95c385222431e3f90e963baea13d3ada5e377cbd
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-04-30 11:31:49 +0200

    deleted file

--- android/common/src/main/java/com/hedvig/app/common/CommonContextExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.hedvig.app.common
-
-import android.app.Activity
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Context.MODE_PRIVATE
-import android.content.Intent
