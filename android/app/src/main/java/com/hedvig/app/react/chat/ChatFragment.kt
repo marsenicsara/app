1730d07e87cfb57cba66f0654f963c8a9fd3c220
Oscar Nylander <oscar@hedvig.com>
2019-05-02 12:40:32 +0200

    Improve the chatfragment code very slightly

--- android/app/src/main/java/com/hedvig/app/react/chat/ChatFragment.kt
+++ android/app/src/main/java/com/hedvig/app/react/chat/ChatFragment.kt
@@ -66,7 +66,7 @@ class ChatFragment : Fragment(), DefaultHardwareBackBtnHandler {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_chat, null)
+        val view = inflater.inflate(R.layout.fragment_chat, container, false)
         val reactRootView = ReactRootView(requireContext())
         this.reactRootView = reactRootView
         view.reactViewContainer.addView(this.reactRootView)
